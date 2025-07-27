7963d5c3fd9d94233d5a955fdd10dd3eb5465be4
Rama-X2 <aderamadhaniputra35@gmail.com>
2025-07-28 02:26:11 +0700

    Remove crypto-browserify dependency

--- next.config.js
+++ next.config.js
@@ -109,7 +109,7 @@ const nextConfig = {
         fs: false,
         net: false,
         tls: false,
-        crypto: require.resolve('crypto-browserify'),
+        crypto: false,
       };
     }
 
